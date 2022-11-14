265131f4daf44a76275114866b9d84c89d3a030e
dongze <dongze@Dongzes-MacBook-Air.local>
2022-11-14 21:40:11 +0800

    update

---
+++ train_scripts/convnext/cifar_100/train_full.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,  python  -m torch.distributed.launch --nproc_per_node=4  --master_port=33518 \
+	train.py /path/to/cifar100 --dataset torch/cifar100 --num-classes 100 --model convnext_base_in22k \
+    --batch-size 32 --epochs 100 \
+	--opt adamw  --weight-decay 0.05 \
+    --warmup-lr 5e-7 --warmup-epochs 10  \
+    --lr 5e-5 --min-lr 5e-8 \
+    --drop-path 0.2 --img-size 224 \
+	--model-ema --model-ema-decay 0.99992  \
+	--output  output/convnext_base_in22k/cifar_100/full \
+	--amp  --pretrained  \
---
+++ train_scripts/vit/cifar_100/train_linear_probe.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,  python  -m torch.distributed.launch --nproc_per_node=4  --master_port=12346  \
+	train.py /path/to/cifar100 --dataset torch/cifar100 --num-classes 100 --model vit_base_patch16_224_in21k  \
+    --batch-size 32 --epochs 100 \
+	--opt adamw  --weight-decay 0.05 \
+    --warmup-lr 1e-7 --warmup-epochs 10  \
+    --lr 1e-3 --min-lr 1e-8 \
+    --drop-path 0 --img-size 224 \
+	--model-ema --model-ema-decay 0.99992  \
+	--output  output/vit_base_patch16_224_in21k/cifar_100/linear_probe \
+	--amp --tuning-mode linear_probe --pretrained  \
